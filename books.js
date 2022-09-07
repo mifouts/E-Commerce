a7821f2d4fa15d793f9ccf43e0a58e0a9ce7cae1
mifouts <fouts.mitch15@gmail.com>
2022-09-07 18:37:11 -0400

    using map to return HTML in

--- books.js
+++ books.js
@@ -2,27 +2,32 @@ function renderBooks(){
   const booksWrapper = document.querySelector('.books');
 
   const books = getBooks();
-  console.log(books)
   
-  booksWrapper.innerHTML =
-  `<div class="book">
-  <figure class="book__img--wrapper">
-    <img class="book__img" src="${books[0].url}" alt="">
-  </figure>
-  <div class="book__title">
-    ${books[0].title}
-  </div>
-  <div class="book__ratings">
-    <i class="fas fa-star"></i>
-    <i class="fas fa-star"></i>
-    <i class="fas fa-star"></i>
-    <i class="fas fa-star"></i>
-    <i class="fas fa-star-half-alt"></i>
-  </div>
-  <div class="book__price">
-    <span class="book__price--normal">$${books[0].originalPrice}</span> $${books[0].salePrice}
-  </div>
-</div>`
+  const booksHtml = books.map((book) => {
+    return `<div class="book">
+    <figure class="book__img--wrapper">
+      <img class="book__img" src="${book.url}" alt="">
+    </figure>
+    <div class="book__title">
+      ${book.title}
+    </div>
+    <div class="book__ratings">
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star-half-alt"></i>
+    </div>
+    <div class="book__price">
+      <span class="book__price--normal">$${book.originalPrice}</span> $${book.salePrice}
+    </div>
+  </div>`;
+  });
+
+  booksWrapper.innerHTML = booksHtml;
+  console.log(booksHtml);
+
+  //booksWrapper.innerHTML =
 }
 
 setTimeout(() => {
