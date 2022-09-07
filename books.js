f1cb0139f95c42d6367dd6d38e3eacb07ef61e1e
mifouts <fouts.mitch15@gmail.com>
2022-09-07 19:16:29 -0400

    making the code dynamic

--- books.js
+++ books.js
@@ -4,10 +4,17 @@ function renderBooks(filter){
   const books = getBooks();
 
   if (filter === 'LOW_TO_HIGH'){
-    console.log(filter)
-    const filteredBooks = books.sort((a,b) => a.originalPrice - b.originalPrice);
-    console.log(filteredBooks);
+    books.sort((a,b) => a.originalPrice - b.originalPrice);
   }
+
+  else if (filter === 'HIGH_TO_LOW'){
+    books.sort((a,b) => b.originalPrice - a.originalPrice);
+  }
+
+  else if (filter === 'RATING') {
+    books.sort((a,b) => b.rating - a.rating);
+  }
+
   
   const booksHtml = books.map((book) => {
     return `<div class="book">
@@ -18,11 +25,7 @@ function renderBooks(filter){
       ${book.title}
     </div>
     <div class="book__ratings">
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star-half-alt"></i>
+    ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
       <span>$${book.originalPrice.toFixed(2)}</span>
@@ -34,6 +37,20 @@ function renderBooks(filter){
 
 
   booksWrapper.innerHTML = booksHtml;
+}
+
+function ratingsHTML(rating) {
+  let ratingHTML = '';
+
+  for (let i = 0; i < Math.floor(rating); i++) {
+    ratingHTML += '<i class="fas fa-star"></i>\n'
+  }
+  
+  if (!Number.isInteger(rating)){
+    ratingHTML += '<i class="fas fa-star-half-alt"></i>\n'
+  }
+
+  return ratingHTML;
 }
 
 function filterBooks(event) {
